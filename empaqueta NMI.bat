287579dfce713c4cd22a30dc56c2b0d229517e5c
dr-slump-0 <69214623+dr-slump-0@users.noreply.github.com>
2023-03-10 22:34:05 +0100

    Add files via upload

---
+++ empaqueta NMI.bat
@@ -0,0 +1,116 @@
+@ECHO OFF
+SET VERSION=20
+SET SRC=C:\Users\david\Desktop\v%VERSION%
+SET DST=C:\Users\david\Desktop\Paquete
+SET PKG1=nmi_v0.0.%VERSION%
+SET PKG2=custom_v0.0.%VERSION%
+SET PKG3=esxdos089
+
+::
+
+if exist "%DST%\%PKG1%\" (
+rd /q /s %DST%\%PKG1%
+)
+mkdir %DST%\%PKG1%
+mkdir %DST%\%PKG1%\BIN
+mkdir %DST%\%PKG1%\SYS
+mkdir %DST%\%PKG1%\SYS\NMI
+
+if exist "%DST%\%PKG2%\" (
+rd /q /s %DST%\%PKG2%
+)
+mkdir %DST%\%PKG2%
+
+if exist "%DST%\%PKG3%\" (
+rd /q /s %DST%\%PKG3%
+)
+XCOPY /E /Q /H /Y "%DST%\%PKG3%-org" "%DST%\%PKG3%\" > NUL
+mkdir %DST%\%PKG3%\SYS\NMI
+
+::
+
+if exist "%DST%\%PKG1%.zip" (
+del /q %DST%\%PKG1%.zip
+)
+if exist "%DST%\%PKG2%.zip" (
+del /q %DST%\%PKG2%.zip
+)
+if exist "%DST%\%PKG3%.zip" (
+del /q %DST%\%PKG3%.zip
+)
+
+::
+
+COPY /Y "%SRC%\_nmi\nmi.sys" "%DST%\%PKG1%\sys\nmi.sys" > NUL
+if errorlevel 1 goto ERROR
+COPY /Y "%SRC%\_nmi\nmi.sys" "%DST%\%PKG3%\sys\nmi.sys" > NUL
+if errorlevel 1 goto ERROR
+echo copied nmi.sys
+
+::for %%i in (hexview,drives,rm) do (
+for %%i in (hexview,drives,rm,ramview,pages) do (
+	COPY /Y "%SRC%\.%%i\%%i" "%DST%\%PKG1%\bin\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	COPY /Y "%SRC%\.%%i\%%i" "%DST%\%PKG3%\bin\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+::for %%i in (config,custom,debug,delete,enter,fastcfg,fastload,help,init,loadold,lock,poke,reload,rename,reset,savesna,seldrv,tapein,tapeout,trd2drv,view) do (
+for %%i in ()
+	COPY /Y "%SRC%\%%i\%%i" "%DST%\%PKG1%\sys\nmi\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	COPY /Y "%SRC%\%%i\%%i" "%DST%\%PKG3%\sys\nmi\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+::for %%i in (nmi.cnf,fastcfg.txt,TRDN.tap,old085.sys,old086.sys,old087.sys,old088.sys,old089.sys,help1.scr,help2.scr,help3.scr) do (
+for %%i in (nmi.cnf,fastcfg.txt,old085.sys,old086.sys,old087.sys,old088.sys,old089.sys,help1.scr,help2.scr,help3.scr,TRND.BAS) do (
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG1%\sys\nmi\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG3%\sys\nmi\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+for %%i in (128,mon,ownrom) do (
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG1%\bin\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG3%\bin\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+for %%i in (rtc.sys) do (
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG1%\sys\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	COPY /Y "%SRC%\_support\%%i" "%DST%\%PKG3%\sys\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+COPY /Y "%SRC%\custom\*.*" "%DST%\%PKG2%\" > NUL
+if errorlevel 1 goto ERROR
+echo copied custom
+
+COPY /Y "%SRC%\_support\README.txt" "%DST%\" > NUL
+if errorlevel 1 goto ERROR
+COPY /Y "%SRC%\_support\README.txt" "%DST%\%PKG1%\" > NUL
+if errorlevel 1 goto ERROR
+COPY /Y "%SRC%\_support\README.txt" "%DST%\%PKG3%\" > NUL
+if errorlevel 1 goto ERROR
+echo copied README.txt
+
+"C:\Program Files\7-Zip\7z.exe" a -r -y "%DST%\%PKG1%.zip" "%DST%\%PKG1%\*" > NUL
+echo created %DST%\%PKG1%.zip
+"C:\Program Files\7-Zip\7z.exe" a -r -y "%DST%\%PKG2%.zip" "%DST%\%PKG2%\*" > NUL
+echo created %DST%\%PKG2%.zip
+"C:\Program Files\7-Zip\7z.exe" a -r -y "%DST%\%PKG3%.zip" "%DST%\%PKG3%\*" > NUL
+echo created %DST%\%PKG3%.zip
+
+PAUSE
+EXIT /B
+
+:ERROR
+PAUSE
---
+++ ensambla NMI.bat
@@ -0,0 +1,143 @@
+@ECHO OFF
+
+SET VERSION=20
+SET RUTA=C:\Users\david\Desktop\v%VERSION%
+SET DISKS=C:\Users\david\Desktop\Images
+SET DRIVE=F:
+
+SET OSFMOUNT="C:\Program Files\OSFMount\OSFMount.com"
+SET RAW2HDF="C:\Users\david\Desktop\Images\raw2hdf"
+SET PSEXEC="C:\Users\david\Desktop\Images\psexec64"
+SET SYNC64="C:\Users\david\Desktop\Images\sync64"
+SET PASMO="C:\Users\david\Desktop\Images\pasmo54b2.exe"
+SET SJASMPLUS="C:\Users\david\Desktop\Images\sjasmplus.exe"
+
+openfiles > NUL 2> NUL
+if %ERRORLEVEL% equ 1 (
+	%PSEXEC% -d -h "%~f0"
+	goto FIN
+)
+
+::
+:: Ensamblamos nmi.sys
+::
+
+del %RUTA%\_nmi\nmi.sys > NUL
+::%PASMO% --err --bin %RUTA%\_nmi\nmi.asm %RUTA%\_nmi\nmi.sys %RUTA%\_nmi\nmi.sym | FIND /I "ERROR"
+%SJASMPLUS% --nologo --syntax=bFi --dirbol --sym=%RUTA%\_nmi\nmi.sym  --raw=%RUTA%\_nmi\nmi.sys %RUTA%\_nmi\nmi.asm 2>&1 | FIND /I "error:"
+if not errorlevel 1 goto ERROR
+echo assembled nmi.asm
+
+::
+:: Creamos los ficheros _nmi\nmi.publics y custom\nmi.publics
+::
+
+::COPY /Y %RUTA%\_nmi\NMI.inc %RUTA%\custom\NMI.inc > NUL
+::if errorlevel 1 goto ERROR
+::echo updated \custom\NMI.inc
+
+del /q %RUTA%\_nmi\nmi.publics
+for %%i in (ovrBuf,clrScr,kUp,kDown,kLeft,kRight,kEnter,prStr,waitKey,prChr,bDAttr,bDName,fUnlink,fChDir,fOpen,fRead,fClose,ckMods,flagSS,flagCS,printError1,fOpen1,fWrite,esxDOSv,msgVer,divRAM,NMIbuf,flg128k,msgRAM1,msgRAM2,restoreScreen,deleteScreen,savedSP,col,pr_64,utoa,saveScreen,fhandle,flgLOCK,curLn,Xof,msgLock,msgDrv,flgROOT,msgPath,fOpenDir,fReadDir,ofY,printError,fTellDir,pCurDir,readKey,prtDec,wait,ldConf,ldDir,exitNMI,pTable) do (
+:: pTable obsolete (v18 and below)
+	findstr /i /r /c:"^%%i\>" %RUTA%\_nmi\nmi.sym >> %RUTA%\_nmi\nmi.publics
+	)
+echo created \nmi\nmi.publics
+
+::del /q %RUTA%\custom\nmi.publics
+::for %%i in (NMIbuf,savedSP,divRAM,esxDOSv,flg128k,bDAttr,bDName,ovrBuf,waitKey,readKey,clrScr,utoa,prStr,prChr,prtDec) do (
+::	findstr /i /r /c:"^%%i\>" %RUTA%\_nmi\nmi.sym >> %RUTA%\custom\nmi.publics
+::	)
+::echo created \custom\nmi.publics
+
+::
+:: Ensamblamos los ovelays auxiliares y dot commands
+::
+
+::for %%i in (hexview,drives,rm,cdrv) do (
+for %%i in (hexview,drives,rm,cdrv,ramview,pages) do (
+	del %RUTA%\.%%i\%%i > NUL
+	::%PASMO% --err --bin %RUTA%\.%%i\%%i.asm %RUTA%\.%%i\%%i | FIND /I "ERROR"
+	%SJASMPLUS% --nologo --syntax=bFiw --dirbol --raw=%RUTA%\.%%i\%%i %RUTA%\.%%i\%%i.asm 2>&1 | FIND /I "error:"
+	if not errorlevel 1 goto ERROR
+	echo assembled %%i.asm
+	)
+
+::for %%i in (config,custom,debug,delete,enter,fastcfg,fastload,help,init,loadold,lock,poke,reload,rename,reset,savesna,seldrv,tapein,tapeout,trd2drv,view) do (
+for %%i in () do (
+	del %RUTA%\%%i\%%i > NUL
+	::%PASMO% --err --bin%RUTA%\%%i\%%i.asm %RUTA%\%%i\%%i | FIND /I "ERROR"
+	%SJASMPLUS% --nologo --syntax=bFiw --dirbol --raw=%RUTA%\%%i\%%i %RUTA%\%%i\%%i.asm 2>&1 | FIND /I "error:"
+	if not errorlevel 1 goto ERROR
+	echo assembled %%i.asm
+	)
+
+::
+:: Copiamos los nuevos ficheros ensamblados a disco
+::
+
+if not exist %DRIVE%\sys\nmi (md %DRIVE%\sys\nmi)
+
+COPY /Y %RUTA%\_nmi\nmi.sys %DRIVE%\sys\nmi.sys > NUL
+if errorlevel 1 goto ERROR
+echo copied nmi.sys
+
+::for %%i in (hexview,drives,rm,cdrv) do (
+for %%i in (hexview,drives,rm,cdrv,ramview,pages) do (
+	COPY /Y %RUTA%\.%%i\%%i %DRIVE%\bin\%%i > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+::for %%i in (config,custom,debug,delete,enter,fastcfg,fastload,help,init,loadold,lock,poke,reload,rename,reset,savesna,seldrv,tapein,tapeout,trd2drv,view) do (
+for %%i in () do (
+	COPY /Y %RUTA%\%%i\%%i %DRIVE%\sys\nmi\%%i > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+::for %%i in (nmi.cnf,fastcfg.txt,TRDN.tap,old085.sys,old086.sys,old087.sys,old088.sys,old089.sys,help1.scr,help2.scr,help3.scr) do (
+for %%i in (nmi.cnf,fastcfg.txt,old085.sys,old086.sys,old087.sys,old088.sys,old089.sys,help1.scr,help2.scr,help3.scr,TRDN.BAS) do (
+	COPY /Y %RUTA%\_support\%%i %DRIVE%\sys\nmi\%%i > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+for %%i in (128,mon,ownrom) do (
+	COPY /Y "%RUTA%\_support\%%i" "%DRIVE%\bin\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+for %%i in (rtc.sys) do (
+	COPY /Y "%RUTA%\_support\%%i" "%DRIVE%\sys\%%i" > NUL
+	if errorlevel 1 goto ERROR
+	echo copied %%i
+	)
+
+::
+:: Sincronizamos disco y actualizamos imagen
+::
+
+echo synchronizing disks...
+::%OSFMOUNT% -d -m f:
+%SYNC64% -r
+echo synchronized.
+
+COPY /Y %DISKS%\32MB.ide %DISKS%\32MB.mmc
+
+echo converting to hdf...
+rem Convertimos imagen raw a formato hdf
+::%RAW2HDF% -v1.0 %DISKS%\mmc089.mmc %DISKS%\mmc089.hdf
+%RAW2HDF% -v1.0 %DISKS%\32MB.ide %DISKS%\mmc089.hdf
+echo converted to hdf
+
+::echo mounting disks...
+::%OSFMOUNT% -a -t file -f %DISKS%\mmc089.mmc -o rw -m f:
+::echo remounted.
+
+::PAUSE
+
+:ERROR
+PAUSE
+
+:FIN
